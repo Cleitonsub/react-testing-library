f06b4b7a7e7f6bb8164e2b33aa6dc0bf5d807ed3
Cleiton <cleitonrmacedo@gmail.com>
2022-01-28 19:50:59 -0300

    feat: Requisito 3 - Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,79 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const withoutFavor = screen.getByText(/No favorite pokemon found/i);
+
+    expect(withoutFavor).toBeInTheDocument();
+  });
+
+  it('é exibido todos os cards de pokémons favoritados na tela', () => {
+    const pokemons = [
+      {
+        id: 25,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Kanto Viridian Forest',
+            map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+          },
+          {
+            location: 'Kanto Power Plant',
+            map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+          },
+        ],
+        summary: 'This intelligent Pokémon roasts hard berries with electricity to make',
+      },
+      {
+        id: 4,
+        name: 'Charmander',
+        type: 'Fire',
+        averageWeight: {
+          value: '8.5',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Alola Route 3',
+            map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+          },
+          {
+            location: 'Kanto Route 3',
+            map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+          },
+          {
+            location: 'Kanto Route 4',
+            map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+          },
+          {
+            location: 'Kanto Rock Tunnel',
+            map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+          },
+        ],
+        summary: 'The flame on its tail shows the strength of its life force. If it is',
+      },
+    ];
+
+    // defaultProps de FavoritePokemons é um array vazio, e para renderizar o pokemon favorito ele recebe os
+    // dados por props, e é renderizado na tela pela função renderFavoritePokemons()
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const favorPokes = screen.getAllByAltText(/is marked as favorite/i);
+
+    expect(favorPokes).toHaveLength(2);
+  });
+});
